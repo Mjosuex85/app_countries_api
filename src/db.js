ce2e33bd718b34ee1ef7b8aca8bcd45f36a389c4
Mario Vidal <mario.josuevp@gmail.com>
2022-09-28 18:08:21 +0200

    .

--- src/db.js
+++ src/db.js
@@ -3,11 +3,11 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, ADRESS, URL_HEROKU
+  DB_USER, DB_PASSWORD, DB_HOST, ADRESS, DATABASE_URL
 } = process.env;
 
 
-const sequelize = new Sequelize(URL_HEROKU, {
+const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   dialectOptions: {
